9616b962f1270f0afca37182ae5c4f8fb179479e
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-08 14:22:26 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,8 @@
 def using_until
   levitation_force = 6
-  counter = 0
-  until counter > levitation_force do
+  until levitation_force do
     puts "Wingardium Leviosa"
-    counter += 1
+    levitation_force += 1
   end
     
 end
